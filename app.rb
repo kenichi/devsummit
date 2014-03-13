6c8b2f01e7a074ac8615636a370938367512270a
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2014-03-13 10:50:59 -0700

    commit all the things

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+require 'bundler'
+Bundler.require
+require 'angelo/tilt/erb'
+
+class Devsummit < Angelo::Base
+  include Angelo::Tilt::ERB
+
+  before do
+    @gt = Geotrigger::Application.new YAML.load_file 'geotrigger.yml'
+  end
+
+  get '/' do
+    @trigger_list = @gt.post 'trigger/list', boundingBox: :geojson
+    @host = 'esri.nakamura.io'
+    erb :index
+  end
+
+  post '/trigger_callback' do
+    websockets.each do |ws|
+      ws.write params['trigger']['triggerId']
+    end
+  end
+
+  socket '/callbacks' do |ws|
+    websockets << ws
+    begin
+      while msg = ws.read do
+        ws.write 'pong' if msg == 'ping'
+      end
+    rescue Reel::SocketError => rse
+      ws.close
+    end
+  end
+
+end
+
+Devsummit.run
